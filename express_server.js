8b0c678bf069d3ea1d05d7a73d04dff93fc59225
vagrant <ttjho@hotmail.com>
2020-09-28 22:03:48 +0000

    add /urls/:shortURL route and urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -14,9 +14,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls : urlDatabase }//this makes an object called templateVars with a single object with the data from the urlDatabase.
+  const templateVars = { urls : urlDatabase }//this makes an object called urls with a single object with the data from the urlDatabase.
   
   res.render("urls_index", templateVars)
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars= { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
