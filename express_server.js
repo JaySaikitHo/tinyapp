b6f52fce64b801e87e876e06746d0dab550ddec2
vagrant <ttjho@hotmail.com>
2020-09-29 00:09:54 +0000

    add body-parser to dependency, add .post path to express_server.js, add random string generator function

--- express_server.js
+++ express_server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  let random = "";
+  let characters = 'abcdefghijklmnopqrstuvwxyz1234567890'
+  for (let i = 0; i < 6; i++){
+    random =+ characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return random;
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -11,6 +23,15 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls", (req, res) => {
