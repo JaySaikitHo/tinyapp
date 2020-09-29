7563a564da53611bf32f7bdcae84b1c139e1f3c7
vagrant <ttjho@hotmail.com>
2020-09-29 18:50:03 +0000

    add delete route to express_servers.js and buttons to urls_index.js

--- express_server.js
+++ express_server.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 
 function generateRandomString() {
   let random = "";
-  let characters = 'abcdefghijklmnopqrstuvwxyz1234567890'
+  const characters = 'abcdefghijklmnopqrstuvwxyz1234567890'
   for (let i = 0; i < 6; i++){
     random += characters.charAt(Math.random() * characters.length);
   }
@@ -20,45 +20,58 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+//landing page
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  // res.send("Ok");// Respond with 'Ok' (we will replace this)
-  let randomString = generateRandomString()
-  const templateVars = { shortURL: randomString, longURL: req.body.longURL}   // don't need params because it is coming from the body not the browser      
-  console.log(templateVars.shortURL)
-  urlDatabase[templateVars.shortURL] = templateVars.longURL;
-  console.log(urlDatabase)
-  res.redirect(`/urls/${templateVars.shortURL}`)
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.get("/urls", (req, res) => {
-  const templateVars = { urls : urlDatabase }//this makes an object called urls with a single object with the data from the urlDatabase.
-  
-  res.render("urls_index", templateVars)
-});
-
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars= { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
-  res.render("urls_show", templateVars);
-});
-
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL] 
-  res.redirect(longURL);
 });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+//update the database
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  // res.send("Ok");// Respond with 'Ok' (we will replace this)
+  let randomString = generateRandomString() // to generate a random id
+  const fullURL = req.body.longURL
+  const templateVars = { shortURL: randomString, longURL: fullURL}   // don't need params because it is coming from the body not the browser      
+  
+  urlDatabase[templateVars.shortURL] = templateVars.longURL;
+ 
+  res.redirect(`/urls/${templateVars.shortURL}`)
+});
+//goes to the form for creating a new URL
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//browse the url database
+app.get("/urls", (req, res) => {
+  const templateVars = { urls : urlDatabase }//this makes an object called urls with a single object with the data from the urlDatabase.
+  console.log(templateVars)
+  res.render("urls_index", templateVars)
+});
+//read a specific url
+app.get("/urls/:shortURL", (req, res) => {
+  const tinyURL = req.params.shortURL
+  const templateVars= { shortURL: tinyURL, longURL: urlDatabase[tinyURL] }
+  res.render("urls_show", templateVars);
+});
+
+//redirect to the website from the shortened URL
+app.get("/u/:shortURL", (req, res) => {
+  const tinyURL = req.params.shortURL
+  const longURL = urlDatabase[tinyURL] 
+  res.redirect(longURL);
+});
+
+app.post("/urls/:url/delete", (req, res) => {
+  
+  delete urlDatabase[req.params.url]
+  console.log(urlDatabase)
+  res.redirect("/urls")
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
