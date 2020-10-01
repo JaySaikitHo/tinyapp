1c870aa4388c0f89a7cb4b13869687f371f6cba1
vagrant <ttjho@hotmail.com>
2020-10-01 21:58:35 +0000

    implement mocha and chai testing, reinstalled to mocha 7.1.1 because of node conflict

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-const { getLoginUser } = require('../helpers.js');
+const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -17,7 +17,7 @@ const testUsers = {
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail( users,"user@example.com")
+    const user = getUserByEmail( testUsers,"user@example.com")
     const expectedOutput = "userRandomID";
     // Write your assert statement here
     assert.equal(user.id,expectedOutput)
@@ -26,11 +26,12 @@ describe('getUserByEmail', function() {
 });
 
 describe('getUserByEmail', function() {
-  it('should return a user with valid email', function() {
-    const user = getUserByEmail( users,"user@example.com")
-    const expectedOutput = "userRandomID";
+  it('should return undefined if there is no valid email', function() {
+    const user = getUserByEmail( testUsers,"abcd@hotmail.com")
+    const expectedOutput = undefined;
     // Write your assert statement here
-    assert.equal(user.id,expectedOutput)
+    assert.equal(user, expectedOutput)
   });
 
 });
+
